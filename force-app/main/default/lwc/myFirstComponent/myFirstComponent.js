98b58dd1524c2467280713849b6632aa9a69f83b
Rajesh <reader.vm@gmail.com>
2023-01-18 11:58:13 +0000

    @wire - Added Partner Card and APEX Class

--- force-app/main/default/lwc/myFirstComponent/myFirstComponent.js
+++ force-app/main/default/lwc/myFirstComponent/myFirstComponent.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class MyFirstComponent extends LightningElement {}
+export default class MyFirstComponent extends LightningElement {
+
+
+    titleValue = 'My First Lightning Web component';
+
+    sessionvalue = 'New Way | Data Binding - Way to access value from JS in HTML!!!'
+
+
+
+}
--- force-app/main/default/lwc/partnerSearch/partnerSearch.js
+++ force-app/main/default/lwc/partnerSearch/partnerSearch.js
@@ -1,8 +1,51 @@
+import { LightningElement, wire} from 'lwc'; // Step 1: Declare that wire function will be used in the code
 
+// Step 2: use @salesforce module to access apex method
+import fetchAllPartnerType from '@salesforce/apex/partnerSearchController.getAllPartnerTypes'; 
 
 export default class PartnerSearch extends LightningElement {
 
 
+   // Data binding
+
+   appdesc = 'Partner Management app helps Sales & Marketing Team to quickly find list of Channel partners having relationship with the organization. These partners have been vetted by Vendor relationship team. Sales & Marketing users can also use the app to share feedback about a partner and find other details like primary point of contact, location, Sale pipeline etc';
+ 
    
-    
+   value = 'inProgress';
+
+   partnerTypes; // a property to hold all partner types from DB
+
+   // property
+   get options() {
+      // array of objects 
+      return [
+           { label: 'Technology Partner', value: '0x1111' },
+           { label: 'Marketing Partner', value: '0x112' },
+           { label: 'Reseller', value: '0x113' },
+           { label: 'Social Media Partner', value: '0x114' },
+           { label: 'Legal Partner', value: '0x115' },
+       ];
+   }
+
+
+   // Step 3: Use @wire method to make a call to the APEX method
+   @wire(fetchAllPartnerType)
+   handleOuput({data, error}) // data & error are system defined properties to hold data received from DB or error when APEX method is called
+   {
+      //Check if data is retrieved 
+      if(data)
+      {
+        this.partnerTypes = data; // copy the data received from DB into a local property  
+        console.log('this.partnerTypes ::' + JSON.stringify(this.partnerTypes));
+      }
+      else if(error)
+      {
+         console.log('Error');
+      }
+   }
+
+   /* Syntax for reference
+      @wire('name of the function ' , 'Input Parameters')
+      property or function // OP 
+   */
 }
