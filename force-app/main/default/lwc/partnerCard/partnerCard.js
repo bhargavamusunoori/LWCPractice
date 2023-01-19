d5ba82cd0586f37b0ffb2120f174c178afc09497
Rajesh <reader.vm@gmail.com>
2023-01-19 11:55:51 +0000

    Added SLDS and partner Card Code

--- force-app/main/default/lwc/partnerCard/partnerCard.js
+++ force-app/main/default/lwc/partnerCard/partnerCard.js
@@ -5,6 +5,14 @@ import { LightningElement, api, wire } from 'lwc';
 export default class PartnerCard extends LightningElement {
 
 
+    partnerProfile = {
+        partnerUserImg : 'https://randomuser.me/api/portraits/med/men/1.jpg',
+        partnerUserCompany : 'Microsoft',
+        partnerUserName : 'Sam Anderson',
+        partnerType : 'Marketing Partner'
+    
+    }
+    
 
 
 
--- force-app/main/default/lwc/partnerSearch/partnerSearch.js
+++ force-app/main/default/lwc/partnerSearch/partnerSearch.js
@@ -16,16 +16,27 @@ export default class PartnerSearch extends LightningElement {
    partnerTypes; // a property to hold all partner types from DB
 
    // property
-   get options() {
+   //get options() {
       // array of objects 
-      return [
+      /*return [
            { label: 'Technology Partner', value: '0x1111' },
            { label: 'Marketing Partner', value: '0x112' },
            { label: 'Reseller', value: '0x113' },
            { label: 'Social Media Partner', value: '0x114' },
            { label: 'Legal Partner', value: '0x115' },
-       ];
-   }
+       ];*/
+
+     /*  return [
+         {"value":"a065j000009ck5oAAA","label":"Marketing Partner"},
+         {"value":"a065j000009ck5pAAA","label":"Resellers"},
+         {"value":"a065j000009ck5qAAA","label":"Recruited Advocates"},
+         {"value":"a065j000009ck5rAAA","label":"Affiliates"},
+         {"value":"a065j000009ck5sAAA","label":"Social Media Channel Partner"},
+         {"value":"a065j000009ck5tAAA","label":"Distributors"},
+         {"value":"a065j000009ck5uAAA","label":"Client Advocates"},
+         {"value":"a065j000009ck5vAAA","label":"Technology Partner"}
+      ]
+   }*/
 
 
    // Step 3: Use @wire method to make a call to the APEX method
@@ -35,8 +46,29 @@ export default class PartnerSearch extends LightningElement {
       //Check if data is retrieved 
       if(data)
       {
-        this.partnerTypes = data; // copy the data received from DB into a local property  
-        console.log('this.partnerTypes ::' + JSON.stringify(this.partnerTypes));
+        
+         console.log('Original data from DB:: ' + JSON.stringify(data));
+
+        // create an object to show as 1st iten
+        this.partnerTypes = [ {'label': 'Select a partner Type', 'value':''}];
+        
+        // loop thru each record retrieved from DB and then change the key
+        data.forEach(item => {
+
+         // switch keys
+         const partnerType = {};
+         partnerType.label = item.Name;
+         partnerType.value = item.Id;
+
+         // add this temp object into main partnerTypes
+         this.partnerTypes.push(partnerType);
+         
+        });
+        
+        
+       //this.partnerTypes = data; // copy the data received from DB into a local property  
+        console.log('this.partnerTypes - After::' + JSON.stringify(this.partnerTypes));
+      
       }
       else if(error)
       {
