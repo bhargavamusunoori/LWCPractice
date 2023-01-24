d2320f636deb39b1b8c7b69fbaa645b9262e5681
Rajesh <reader.vm@gmail.com>
2023-01-24 12:04:45 +0000

    Modified partner Search Result and partner Search

--- force-app/main/default/lwc/partnerCard/partnerCard.js
+++ force-app/main/default/lwc/partnerCard/partnerCard.js
@@ -5,13 +5,65 @@ import { LightningElement, api, wire } from 'lwc';
 export default class PartnerCard extends LightningElement {
 
     // Exposing a public property 
-    @api partnerProfile; 
+    @api partnerProfile;
 
-   
+    userImg;
+    partnerTheme;
+    partnerTitleTheme; 
 
-   
+    connectedCallback() {
+        this.getUserImage(this.partnerProfile.Partner_Primary_POC__r.Salutation);
+        this.getPartnerTheme(this.partnerProfile.Partner_Type_Lookup__r.Name);
+    }
 
-    
+    getUserImage(title) {
+        const randomId = Math.floor(Math.random() * 100);
+
+        switch (title) {
+            case 'Mr.':
+                this.userImg = `https://randomuser.me/api/portraits/med/men/${randomId}.jpg`;
+                break;
+            case 'Ms.':
+                this.userImg = `https://randomuser.me/api/portraits/med/women/${randomId}.jpg`
+                break;
+            default:
+                this.userImg = 'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg'
+                break;
+        }
+    }
+
+    getPartnerTheme(PartnerType) {
+        switch (PartnerType) {
+            case "Technology Partner":
+                this.partnerTheme = 'slds-theme_success pointer';
+                this.partnerTitleTheme = 'slds-align_absolute-center title'
+
+                break;
+            case "Marketing Partner":
+                this.partnerTheme = 'slds-theme_info pointer';
+                this.partnerTitleTheme = 'titleStyleForMarketingPartner slds-align_absolute-center title'
+                break;
+            case "Affiliates":
+                this.partnerTheme = 'slds-theme_warning pointer';
+                this.partnerTitleTheme = 'slds-align_absolute-center title'
+
+                break;
+            case "Client Advocates":
+                this.partnerTheme = 'slds-theme_error pointer';
+                this.partnerTitleTheme = 'slds-align_absolute-center title'
+
+                break;
+            default:
+                this.partnerTheme = 'slds-theme_offline pointer';
+                this.partnerTitleTheme = 'slds-align_absolute-center title'
+
+                break;
+        }
+    }
+
+
+
+
 
 
 
--- force-app/main/default/lwc/partnerSearchResult/partnerSearchResult.js
+++ force-app/main/default/lwc/partnerSearchResult/partnerSearchResult.js
@@ -1,11 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getPartners from '@salesforce/apex/partnerSearchController.getPartners';
 
 
 export default class PartnerSearchResult extends LightningElement {
 
 
 
-  ChannelPartnersList =  [
+ /* ChannelPartnersList =  [
 
     {Id:1,partnerUserImg : 'https://randomuser.me/api/portraits/med/women/1.jpg',partnerUserCompany : 'Oracle',partnerUserName : 'Amy Anderson',partnerType : 'Technology Partner'}
     ,{Id:2,partnerUserImg : 'https://randomuser.me/api/portraits/med/men/1.jpg',partnerUserCompany : 'Microsoft',partnerUserName : 'Samuel Jackson',partnerType : 'Technology Partner'}
@@ -13,17 +14,32 @@ export default class PartnerSearchResult extends LightningElement {
     ,{Id:4,partnerUserImg : 'https://randomuser.me/api/portraits/med/men/10.jpg',partnerUserCompany : '6sense',partnerUserName : 'John Doe',partnerType : 'Technology Partner'}
     ,{Id:5,partnerUserImg : 'https://randomuser.me/api/portraits/med/men/11.jpg',partnerUserCompany : 'mailchimp',partnerUserName : 'Jack Sparrow',partnerType : 'Marketing Partner'}
 
-  ];
+  ];*/
+
+  ChannelPartnersList;
+
+  partnerTypeId = ''; //= 'a065j000009ck5oAAA';
+
+  @wire(getPartners, { strPartnerTypeId: '$partnerTypeId' }) // param1: Name of the APEX method; param2: Inputs parameters to APEX method 
+  processOutput({ data, error }) {
+    if (data) {
+      console.log('Channel Partners::' + JSON.stringify(data));
+      // assign the data coming from the DB
+      this.ChannelPartnersList = data;
+    }
+    else if(error)
+    {
+      console.log('Error!!!')
+    }
+  }
 
   // property to check if data is available 
-  get isPartnersAvailable()
-  {
-    if(this.ChannelPartnersList !=null && this.ChannelPartnersList.length > 0)
-    {
-        return true;
+  get isPartnersAvailable() {
+    if (this.ChannelPartnersList != null && this.ChannelPartnersList.length > 0) {
+      return true;
     }
     return false;
   }
-   
+
 
 }
